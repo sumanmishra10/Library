7d58661094d587003fea9e26d9f6643e38743a72
suman <beingsuman10@gmail.com>
2024-06-25 23:05:39 +0545

    push

--- LibraryWeb/wwwroot/js/product.js
+++ LibraryWeb/wwwroot/js/product.js
@@ -5,19 +5,21 @@ $(document).ready(function () {
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url : '/admin/product/getall' },
+        "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'name', "width" : "15%" },
+            { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
-            { data: 'listPrice', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
-                    return '<div>Hello</div>'
-                },
-                "width": "15%"
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                "width": "25%"
             }
         ]
     });
